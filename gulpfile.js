55bbc24659ccab7dcb976ef42b1a87edd3070363
Aleksey Zinovjev <alexz04tab2@gmail.com>
2024-11-19 01:00:07 +0700

    add less with gulp

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+const gulp = require('gulp');
+const sourcemaps = require('gulp-sourcemaps');
+const less = require('gulp-less');
+
+const browserSync = require('browser-sync').create();
+
+const lessFiles = [
+    './src/styles/test.less'
+]
+
+function styles() {
+    return gulp.src(lessFiles)
+    .pipe(sourcemaps.init())
+    .pipe(less())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./build/style'))
+    .pipe(browserSync.stream());
+}
+
+function watch() {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    });
+    gulp.watch('./src/styles/**/*.less', styles);
+    gulp.watch('./*.less').on('change', browserSync.reload);
+    gulp.watch('./*.html').on('change', browserSync.reload);
+}
+
+gulp.task('styles', styles);
+gulp.task('watch', watch);
+
+gulp.task('dev', gulp.series('styles','watch'));
