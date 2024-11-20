ce2f5b61aad413f2160f9f62ad6e93e14b01a90b
Aleksey Zinovjev <alexz04tab2@gmail.com>
2024-11-20 12:11:03 +0700

    add registration form style

--- src/js/passwordControl.js
+++ src/js/passwordControl.js
@@ -1,13 +1,18 @@
 const pasControl = document.querySelector('.password-control');
-const input = document.getElementById('password_login');
+const inputPas = document.getElementById('password_login');
+const inputPasRepeat = document.getElementById('password_login_repeat');
 
 pasControl.addEventListener("click", () => {
     if (pasControl.classList.contains('view')) {
         pasControl.classList.remove('view');
-        input.setAttribute('type', 'password');
+        inputPas.setAttribute('type', 'password');
+
+        if (inputPasRepeat != null) inputPasRepeat.setAttribute('type', 'password');
     }
     else {
         pasControl.classList.add('view');
-        input.setAttribute('type', 'text');
+        inputPas.setAttribute('type', 'text');
+
+        if (inputPasRepeat != null) inputPasRepeat.setAttribute('type', 'text');
     }
 });
