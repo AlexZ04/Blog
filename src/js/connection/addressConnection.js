67fa60e09eecc788a39c6a9a051dcfb95d2a0b0d
Aleksey Zinovjev <alexz04tab2@gmail.com>
2024-12-05 01:09:33 +0700

    add adressConnection

---
+++ src/js/connection/addressConnection.js
@@ -0,0 +1,44 @@
+import { API_URL } from "../constants.js";
+
+export async function SearchAdress(parentObjectId, query) {
+    const params = new URLSearchParams();
+
+    if (parentObjectId) params.append("parentObjectId", parentObjectId);
+    if (query) params.append("query", query);
+
+    var url = API_URL + `/api/address/search?${params.toString()}`;
+
+    try {
+        let response = await fetch(url);
+
+        if (response.ok) {
+            let data = await response.json();
+
+            return data;
+        }
+    }
+    catch (e) {
+        console.log(e);
+    }
+}
+
+export async function GetChain(objectGuid) {
+    const params = new URLSearchParams();
+
+    if (objectGuid) params.append("objectGuid", objectGuid);
+
+    var url = API_URL + `/api/api/address/chain?${params}`;
+
+    try {
+        let response = await fetch(url);
+
+        if (response.ok) {
+            let data = await response.json();
+
+            return data;
+        }
+    }
+    catch (e) {
+        console.log(e);
+    }
+}
