5a153e9cdbd43592e73c35f93ef4fa51a965cad5
Aleksey Zinovjev <alexz04tab2@gmail.com>
2024-12-12 01:57:45 +0700

    fix address searching

--- src/js/createPost.js
+++ src/js/createPost.js
@@ -1,4 +1,5 @@
 import * as addressConnection from "./connection/addressConnection.js";
+import { TAG_MAP } from "./connection/tagConnection.js";
 
 $(document).ready(function() {
     $('.select2').select2();
@@ -11,21 +12,14 @@ const adressSelectionTemplate = document.getElementById('adress_selection_templa
 setSelect();
 
 async function setSelect(parentId, query) {
-    var addressInfo = await addressConnection.SearchAdress(parentId, query);
-
     var newSelect = adressSelectionTemplate.content.cloneNode(true);
 
     var selectHeader = newSelect.querySelector('p');
 
     var selectField = newSelect.querySelector('.select2');
+    selectField.dataset.parent = parentId;
 
-    var addMap = new Map();
-
-    addressInfo.forEach(element => {
-        if (!addMap.get(element.objectLevelText)) addMap.set(element.objectLevelText, []);
-
-        addMap.get(element.objectLevelText).push(element);
-    });
+    var addMap = await getOpt(parentId, query);
 
     for (let subjectText of addMap.keys()) {
 
@@ -64,9 +58,7 @@ async function setSelect(parentId, query) {
             addressSelectionContainer.removeChild(children);
         });
 
-        if (Array.from(addressSelectionContainer.children).length > 2 || 
-            addressSelectionContainer.firstElementChild.nextElementSibling.querySelector('select').value != "-")
-
+        if (selectField.value !== "-")
             setSelect(selectField.options[selectField.selectedIndex].dataset.index);
 
         selectHeader.textContent = selectField.options[selectField.selectedIndex].dataset.objectLevel;
@@ -74,7 +66,78 @@ async function setSelect(parentId, query) {
         if (selectField.value === "-") selectHeader.textContent = 'Следующий элемент адреса';
     }
 
+    if (Array.from(addMap.keys()).length !== 0)
     addressSelectionContainer.appendChild(newSelect);
 
     $('.select2').select2();
+    $('.select2').on('select2:open', function(e) {
+        var searchField = $(this).data('select2').$dropdown.find('input').get(0);
+        
+        searchField.oninput = async function() {
+            await updateOptions(selectField, searchField.value)
+        }
+    });
+
+    $('.select2').on('select2:close', function(e) {
+        var searchField = $(this).data('select2').$dropdown.find('input').get(0);
+        
+        searchField.oninput = async function() {
+            await updateOptions(selectField)
+        }
+    });
+}
+
+const select = document.querySelector('.tag-select');
+
+TAG_MAP.keys().forEach(element => {
+    var option = document.createElement('option');
+    option.text = element;
+
+    select.appendChild(option);
+});
+
+$(window).on('resize', function () {
+    $('.select2').select2();
+});
+
+async function updateOptions(select, text) {
+    select.innerHTML = "";
+
+    var addMap = await getOpt(select.dataset.parent, text);
+
+    var newOption = document.createElement('option');
+    newOption.textContent = "-";
+    select.appendChild(newOption);
+
+    for (let subjectText of addMap.keys()) {
+
+        var newOptGroup = document.createElement('optgroup');
+        newOptGroup.label = subjectText;
+        
+        addMap.get(subjectText).forEach(element => {
+            var newOption = document.createElement('option');
+            newOption.textContent = element.text;
+            newOption.dataset.index = element.objectId;
+            newOption.dataset.objectLevel = element.objectLevelText;
+
+            newOptGroup.appendChild(newOption);
+        });
+
+        select.appendChild(newOptGroup);
+    }
+
+}
+
+async function getOpt(parentId, query) {
+    var addressInfo = await addressConnection.SearchAdress(parentId, query);
+
+    var addMap = new Map();
+
+    addressInfo.forEach(element => {
+        if (!addMap.get(element.objectLevelText)) addMap.set(element.objectLevelText, []);
+
+        addMap.get(element.objectLevelText).push(element);
+    });
+
+    return addMap;
 }
