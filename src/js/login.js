99553e9b97c985a918a1363f7b174d849fb34a60
Aleksey Zinovjev <alexz04tab2@gmail.com>
2024-12-12 23:27:16 +0700

    add login validation

--- src/js/login.js
+++ src/js/login.js
@@ -1,12 +1,27 @@
 import * as usersConnection from "./connection/usersConnection.js";
+import { delay } from "./delay.js";
 
 const email = document.getElementById('email_login');
 const passwprd = document.getElementById('password_login');
 const loginButton = document.getElementById('enter_login_btn');
 
 loginButton.addEventListener('click', async () => {
-    var inpEmail = email.value;
-    var inpPas = passwprd.value;
+    var inpEmail = email.value.trim();
+    var inpPas = passwprd.value.trim();
+
+    if (inpEmail.length < 1) {
+        email.classList.add('anim');
+        delay(500).then(() => email.classList.remove('anim'));
+        return;
+    }
+
+    if (inpPas.length < 1) {
+        passwprd.classList.add('anim');
+        document.querySelector('.password-control').classList.add('anim');
+        delay(500).then(() => document.querySelector('.password-control').classList.remove('anim'));
+        delay(500).then(() => passwprd.classList.remove('anim'));
+        return;
+    }
 
     var res = await usersConnection.LoginUser(inpEmail, inpPas);
 
